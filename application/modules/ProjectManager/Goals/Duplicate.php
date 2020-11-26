a0f3ab16fc15fbc29314cf731c2d021b7a7b95ea
Ayoola FALOLA <ayoola@ayoo.la>
2020-11-26 20:55:18 +0100

    Initial commit

---
+++ application/modules/ProjectManager/Goals/Duplicate.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * PageCarton
+ *
+ * LICENSE
+ *
+ * @category   PageCarton
+ * @package    ProjectManager_Goals_Abstract
+ * @copyright  Copyright (c) 2019 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: Abstract.php Monday 16th of December 2019 09:10AM ayoola@ayoo.la $
+ */
+
+/**
+ * @see PageCarton_Widget
+ */
+
+
+class ProjectManager_Goals_Duplicate extends ProjectManager_Goals_Creator
+{
+
+    /**
+     * Performs the whole widget running process
+     * 
+     */
+	public function init()
+    {
+        try {
+            //  Code that runs the widget goes here...
+            $this->createForm('Submit...', 'Duplicate a project goal');
+            $this->setViewContent($this->getForm()->view());
+
+            //	self::v( $_POST );
+            if (! $values = $this->getForm()->getValues()) {
+                return false;
+            }
+
+            $goal = ProjectManager_Goals::getInstance()->selectOne( null, array( 'goals_id' => $values['preset'] ) );   
+            $tasks = ProjectManager_Tasks::getInstance()->select( null, array( 'goals_id' => $values['preset'] ) );   
+            //    var_export( $tasks );
+            $goalInfo = $goal;
+            unset( $goalInfo['goals_id'] );
+            $class = new ProjectManager_Goals_Creator( array( 'fake_values' => $goalInfo ) );
+            $class->initOnce();
+            $this->setViewContent( $class->view(), true  ); 
+            if( $goalId = $class->insertInfo )
+            {
+                foreach( $tasks as $task )
+                {
+                    $taskInfo = $task;
+                    unset( $taskInfo['tasks_id'] );
+                    $taskInfo['goals_id'] = $goalId['goals_id'];
+                    $taskInfo['time'] = time();
+                    //    var_export( $taskInfo );
+                    $class = new ProjectManager_Tasks_Creator( array( 'fake_values' => $taskInfo ) );
+                    $class->initOnce();
+                    //    var_export( $class->getForm()->getValues() );
+                    //    var_export( $class->getForm()->getBadnews() );
+                    $this->setViewContent( $class->view()  ); 
+
+                }
+            }
+
+            // end of widget process
+        } catch (Exception $e) {
+            //  Alert! Clear the all other content and display whats below.
+            $this->setViewContent(self::__('<p class="badnews">' . $e->getMessage() . '</p>'));
+            $this->setViewContent(self::__('<p class="badnews">Theres an error in the code</p>'));
+            return false;
+        }
+    }
+
+    /**
+     * creates the form for creating and editing page
+     * 
+     * param string The Value of the Submit Button
+     * param string Value of the Legend
+     * param array Default Values
+     */
+	public function createForm( $submitValue = null, $legend = null, Array $values = null )  
+    {
+		//	Form to create a new page
+        $form = new Ayoola_Form( array( 'name' => $this->getObjectName(), 'data-not-playable' => true ) );
+		$form->submitValue = $submitValue ;
+
+		$fieldset = new Ayoola_Form_Element;
+
+        $options = array();
+
+        $goals = ProjectManager_Goals::getInstance()->select();
+        foreach( $goals as $goal )
+        {
+            if( empty( $goal['article_url'] ) )
+            {
+                continue;
+            }
+            $projectInfo = Application_Article_Abstract::loadPostData( $goal['article_url'] );
+            if( empty( $projectInfo['article_title'] ) )
+            {
+                continue;
+            }
+            $options[$goal['goals_id']] = $goal['goal'] . ' (' . $projectInfo['article_title']  . ')';
+        }
+        asort( $options );
+        $options = array( '' => 'Please Select...' ) + $options;
+
+        $fieldset->addElement( array( 'name' => 'preset', 'label' => 'Duplicate Exist Project Goal', 'type' => 'Select', 'value' => @$values['preset'] ), $options );
+		if( empty( $_GET['article_url'] ) )
+		{
+			$fieldset->addElement( array( 'name' => 'article_url', 'type' => 'InputText', 'value' => @$values['article_url'] ) ); 
+		}
+
+		$fieldset->addLegend( $legend );
+		$form->addFieldset( $fieldset );   
+		$this->setForm( $form );
+    } 
+
+	// END OF CLASS
+}
---
+++ application/pages/includes/post-viewer-managed-project.php
@@ -0,0 +1,73 @@
+<?php
+/**
+* PageCarton Page Generator
+*
+* LICENSE
+*
+* @category PageCarton
+* @package /post-viewer-managed-project
+* @generated Ayoola_Page_Editor_Layout
+* @copyright  Copyright (c) PageCarton. (http://www.PageCarton.com)
+* @license    http://www.PageCarton.com/license.txt
+* @version $Id: post-viewer-managed-project.php	Tuesday 17th of December 2019 12:27:17 PM	ayoola@ayoo.la $ 
+*/
+//	Page Include Content
+
+							if( Ayoola_Loader::loadClass( 'Ayoola_Page_Editor_Text' ) )
+							{
+								
+$_4004aa46eaba8c3d0e7e503066213086 = new Ayoola_Page_Editor_Text( array (
+  'codes' => '<a href="{{{post_link}}}"><img class="xpc_give_space_top_bottom" style="width:100%;" alt="" src="{{{document_url_cropped}}}"></a>
+
+<div class="container pc_container">
+    <h3 class="pc_give_space_top_bottom"><a href="{{{post_link}}}">{{{article_title}}}</a></h3>
+    <p class="pc_give_space_top_bottom"><a target="_blank" class="a2a_dd btn btn-default" addthis:title="{{{article_title}}}" addthis:description="{{{article_description}}}" addthis:url="{{{post_full_url}}}" href="https://www.addtoany.com/share?url={{{post_full_url}}}&amp;title={{{post_type}}} - {{{article_title}}}">
+<span style="font-size:small;"> Share </span> <i class="fa fa-facebook"></i> <i class="fa fa-twitter"></i> <i class="fa fa-whatsapp"></i> <i class="fa fa-share-alt"></i> </a></p>
+    <p style="font-size:small;" class="pc_give_space_top_bottom"><a href="/project-manager/index.php/music/posts/{{{post_type_id}}}">{{{post_type}}}</a> by <a href="/project-manager/index.php/music/{{{profile_url}}}">{{{display_name}}}</a> in {{{category_text}}}</p>
+       
+    <em class="pc_give_space_top_bottom">{{{article_description}}}</em>
+    <div class="pc_give_space_top_bottom">{{{article_content}}}</div>
+    <div class=""><a href="{{{post_link}}}">View <i class="fa fa-eye pc_give_space"></i> {{{views_count_total}}}   <i class="fa fa-comment pc_give_space"></i> {{{comments_count_total}}} </a></div>
+    <div class="pc_give_space_top_bottom"><span style="font-size:x-small;"><i class="fa fa-clock-o "></i> {{{article_creation_date_filtered}}}</span></div>
+</div>',
+  'preserved_content' => '',
+  'url_prefix' => '/project-manager/index.php',
+  'widget_options' => 
+  array (
+    0 => 'embed_widgets',
+    1 => 'parameters',
+    2 => 'savings',
+  ),
+  'savedwidget_id' => '',
+  'pagewidget_id_switch' => '',
+  'widget_name' => '&nbsp; {{{article_title}}}&nbsp; Share &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {{{post_type}}} by {{{disp - 309',
+  'pagewidget_id' => '1576585577-0-2',
+  'markup_template_object_name' => 
+  array (
+    0 => 'Application_Article_View',
+  ),
+  'advanced_parameter_value' => 
+  array (
+    0 => '1',
+    1 => '1800',
+    2 => '1500',
+    3 => '1',
+    4 => '1',
+    5 => '6',
+  ),
+  'pagination[0]' => '1',
+  'cover_photo_width' => '1800',
+  'cover_photo_height' => '1500',
+  'skip_ariticles_without_cover_photo[0]' => '1',
+  'add_a_new_post[0]' => '1',
+  'no_of_post_to_show[0]' => '6',
+) );
+
+							}
+							else
+							{
+								
+$_4004aa46eaba8c3d0e7e503066213086 = null;
+
+							}
+							
